17e33291ad4bbcc959f90e69eaea6b3ac1c029ba
jmndzv <jmndzv@gmail.com>
2021-07-04 14:48:34 -0500

    Create CommercialPlanRangeService.cs
    cambios

---
+++ Fcalc.Application/CommercialPlanRangeService.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Collections.Generic;
+using System;
+using FCalc.Domain.Model.Abstractions;
+using FCalc.DataAccess.Repository;
+using FCalc.Domain.Model.Entities;
+
+namespace FCalc.Application.Service
+{
+    public class CommercialPlanRange
+    {
+        readonly ICommercialPlanRangeRepository repository;
+
+        public CommercialPlanRange()
+        {
+            repository = new CommercialPlanRangeRepository();
+        }
+
+        public IEnumerable<CommercialPlanRange> GetCommercialPlanRange()
+        {
+            return repository.GetAll();
+        }
+
+        public IEnumerable<CommercialPlanRange> FindActiveCommercialPlanRange()
+        {
+            return repository.FindActiveCommercialPlanRange();
+        }
+
+        public void InsertCommercialPlanRange(CommercialPlanRange commercialPlanRange)
+        {
+            repository.Add(commercialPlanRange);
+        }
+
+        public void ModifyCommercialPlanRange(CommercialPlanRange commercialPlanRange)
+        {
+            repository.Modify(commercialPlanRange);
+        }
+
+    }
+}
