6a411c692c8645020f9f491648923d90d5b191f9
jmndzv <jmndzv@gmail.com>
2021-07-04 13:22:53 -0500

    Create CommercialPlanRepository.cs
    CustomerRepository

---
+++ Fcalc.Infra.DataAccess/Repository/CommercialPlanRepository.cs
@@ -0,0 +1,34 @@
+﻿
+using Fcalc.Infra.DataAccess;
+using FCalc.Domain.Model.Abstractions;
+using FCalc.Domain.Model.Entities;
+using FCalc.Infra.DataAccess.Repository;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace FCalc.DataAccess.Repository
+{
+    public class CustomerRepository : BaseRepository<Customer>, ICustomerRepository
+    {
+        public IEnumerable<Customer> FindActiveCustomers()
+        {
+            try
+            {
+               
+                using (FcalcDBEntities1 context = new FcalcDBEntities1())
+                {
+                    var query = from c in context.Customer 
+                                where c.status == "1" 
+                                select c;
+
+                    return query.ToList();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("No se puede procesar consulta", e);
+            }
+        }
+    }
+}
