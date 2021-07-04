2c769d5d7416c206b25ddb013414bb6f30fe257e
jmndzv <jmndzv@gmail.com>
2021-07-04 12:55:16 -0500

    Create ICommercialPlanRangeRepository.cs
    CommercialPlanRange

---
+++ FCalc.Domain.Model/Abstractions/ICommercialPlanRangeRepository.cs
@@ -0,0 +1,11 @@
+﻿using FCalc.Domain.Model.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FCalc.Domain.Model.Abstractions
+{
+    public interface ICommercialPlanRangeRepository:IBaseRepository<CommercialPlanRange>
+    {
+        IEnumerable<CommercialPlanRange> FindActiveCommercialPlanRange();
+    }
+}
