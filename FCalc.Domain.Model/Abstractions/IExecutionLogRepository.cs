95a240b55bf25401ba788aa6b4056a7628e7c43c
jmndzv <jmndzv@gmail.com>
2021-07-04 13:03:06 -0500

    Create IExecutionLogRepository.cs
    ExecutionLog

---
+++ FCalc.Domain.Model/Abstractions/IExecutionLogRepository.cs
@@ -0,0 +1,11 @@
+﻿using FCalc.Domain.Model.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FCalc.Domain.Model.Abstractions
+{
+    public interface IExecutionLogRepository : IBaseRepository<ExecutionLog>
+    {
+        IEnumerable<ExecutionLog> FindActiveExecutionLog();
+    }
+}
